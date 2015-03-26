2242dc5a389a73d6b4fdf6f71d58a9a441f7d267
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2015-03-26 03:32:29 -0700

    added first rough implementation of the similarity search api
    Merge remote-tracking branch 'upstream/master'
    
    Conflicts:
    	msdk-data/src/main/java/io/github/msdk/datamodel/impl/SimpleDataPoint.java
    	msdk-interfaces/src/main/java/io/github/msdk/datamodel/rawdata/DataPoint.java
    	msdk-interfaces/src/main/java/io/github/msdk/datamodel/rawdata/RawDataFileType.java

--- mdk-mona/src/main/java/edu/ucdavis/fiehnlab/mona/MonaSearch.java
+++ mdk-mona/src/main/java/edu/ucdavis/fiehnlab/mona/MonaSearch.java
@@ -1,11 +1,15 @@
 package edu.ucdavis.fiehnlab.mona;
 
+import edu.ucdavis.fiehnlab.mona.pojo.Result;
+import edu.ucdavis.fiehnlab.mona.pojo.SimilaritySearch;
 import io.github.msdk.datamodel.rawdata.MassSpectrum;
 import io.github.msdk.query.Search;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * implementation of the search interface, with mona specific options
@@ -24,6 +28,29 @@ public class MonaSearch implements Search {
 
     @Override
     public Iterator<MassSpectrum> findSimilarSpectra(MassSpectrum compare, Double minSimilarity) {
-        return null;
+
+        SimilaritySearch similaritySearch = new SimilaritySearch();
+
+        List<Result> results = similaritySearch.getResult();
+
+        final Iterator<Result> resultIterator = results.iterator();
+
+        final Iterator<MassSpectrum> iterator = new Iterator<MassSpectrum>() {
+            @Override
+            public boolean hasNext() {
+                return resultIterator.hasNext();
+            }
+
+            @Override
+            public MassSpectrum next() {
+                try {
+                    return findSpectrumById(resultIterator.next().getId());
+                } catch (IOException e) {
+                    throw new RuntimeException(e.getMessage(), e);
+                }
+            }
+        };
+
+        return iterator;
     }
 }
