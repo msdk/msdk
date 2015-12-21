d223aca4d5f2f485c9de79a674c562ce11707d41
Tomas Pluskal <plusik@gmail.com>
2015-12-20 21:48:17 -0500

    Minor fix

--- msdk-rawdata/msdk-rawdata-xic/src/main/java/io/github/msdk/rawdata/xic/MSDKXICMethod.java
+++ msdk-rawdata/msdk-rawdata-xic/src/main/java/io/github/msdk/rawdata/xic/MSDKXICMethod.java
@@ -136,7 +136,6 @@ public class MSDKXICMethod implements MSDKMethod<Chromatogram> {
                         "Invalid chromatogram type: " + chromatogramType);
             }
 
-            processedScans++;
         }
 
         result.setDataPoints(chrDataPoints);
