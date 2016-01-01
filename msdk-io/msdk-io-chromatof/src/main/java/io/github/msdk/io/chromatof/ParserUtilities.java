a9a6d802483aae582912b9309eeba72fb3036bde
nilshoffmann <nhoffmann@netbeans.org>
2016-01-01 11:08:13 +0100

    Added code and test data from Maltcms for ChromaTof Import. Added Rule ExtractClassPathFiles for tests that use compressed (gzipped or zipped) files that take up considerably less space in the git repository.

---
+++ msdk-io/msdk-io-chromatof/src/main/java/io/github/msdk/io/chromatof/ParserUtilities.java
@@ -0,0 +1,134 @@
+/*
+ * Maltcms, modular application toolkit for chromatography-mass spectrometry. 
+ * Copyright (C) 2008-2014, The authors of Maltcms. All rights reserved.
+ *
+ * Project website: http://maltcms.sf.net
+ *
+ * Maltcms may be used under the terms of either the
+ *
+ * GNU Lesser General Public License (LGPL)
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * or the
+ *
+ * Eclipse Public License (EPL)
+ * http://www.eclipse.org/org/documents/epl-v10.php
+ *
+ * As a user/recipient of Maltcms, you may choose which license to receive the code 
+ * under. Certain files or entire directories may not be covered by this 
+ * dual license, but are subject to licenses compatible to both LGPL and EPL.
+ * License exceptions are explicitly declared in all relevant files or in a 
+ * LICENSE file in the relevant directories.
+ *
+ * Maltcms is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. Please consult the relevant license documentation
+ * for details.
+ */
+package io.github.msdk.io.chromatof;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.TreeMap;
+import javafx.util.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Generic utility methods for csv parsing and numeric conversion.
+ *
+ */
+public class ParserUtilities {
+    
+    private static final Logger log = LoggerFactory.getLogger(ParserUtilities.class);
+
+    /**
+     * Parse a numeric string using the specified locale. When a ParseException
+     * is caught, the method returns Double.NaN.
+     *
+     * @param s the string to parse.
+     * @param locale the locale to use for numeric conversion.
+     * @return the double value. May be NaN if s is null, empty, or unparseable
+     */
+    public static double parseDouble(String s, Locale locale) {
+        if (s == null || s.isEmpty()) {
+            return Double.NaN;
+        }
+        try {
+            return NumberFormat.getNumberInstance(locale).parse(s).doubleValue();
+        } catch (ParseException ex) {
+            try {
+                return NumberFormat.getNumberInstance(Locale.US).parse(s).
+                        doubleValue();
+            } catch (ParseException ex1) {
+                return Double.NaN;
+            }
+        }
+    }
+
+    public static HashMap<String, String> getFilenameToGroupMap(File f, String fieldSeparator) {
+        List<String> header = null;
+        HashMap<String, String> filenameToGroupMap = new LinkedHashMap<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(f))) {
+            String line = "";
+            int lineCount = 0;
+            while ((line = br.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    String[] lineArray = line.split(String.valueOf(fieldSeparator));
+                    if (lineCount > 0) {
+                        //                        System.out.println(
+                        //                                "Adding file to group mapping: " + lineArray[0] + " " + lineArray[1]);
+                        filenameToGroupMap.put(lineArray[0], lineArray[1]);
+                    }
+                    lineCount++;
+                }
+            }
+        } catch (IOException ex) {
+            log.warn("Caught an IO Exception while reading file " + f, ex);
+        }
+        return filenameToGroupMap;
+    }
+
+    /**
+     * Method to convert a mass spectrum in the format contained in ChromaTOF
+     * peak files as pairs of mz and intensity, separated by space :
+     * {@code 102:956 107:119}.
+     *
+     * @param massSpectrum the mass spectrum string to parse.
+     * @return a tuple of double[] masses and int[] intensities.
+     */
+    public static Pair<double[], int[]> convertMassSpectrum(
+            String massSpectrum) {
+        if (massSpectrum == null) {
+            log.warn("Warning: mass spectral data was null!");
+            return new Pair<>(new double[0], new int[0]);
+        }
+        String[] mziTuples = massSpectrum.split(" ");
+        TreeMap<Float, Integer> tm = new TreeMap<>();
+        for (String tuple : mziTuples) {
+            if (tuple.contains(":")) {
+                String[] tplArray = tuple.split(":");
+                tm.put(Float.valueOf(tplArray[0]), Integer.valueOf(tplArray[1]));
+            } else {
+                log.warn("Warning: encountered strange tuple: {} within ms: {}", new Object[]{tuple, massSpectrum});
+            }
+        }
+        double[] masses = new double[tm.keySet().size()];
+        int[] intensities = new int[tm.keySet().size()];
+        int i = 0;
+        for (Float key : tm.keySet()) {
+            masses[i] = key;
+            intensities[i] = tm.get(key);
+            i++;
+        }
+        return new Pair<>(masses, intensities);
+    }
+}
---
+++ msdk-test/src/main/java/io/github/msdk/test/ZipResourceExtractor.java
@@ -0,0 +1,146 @@
+/* 
+ * (C) Copyright 2015 by MSDK Development Team
+ *
+ * This software is dual-licensed under either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+package io.github.msdk.test;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * ZipResourceExtractor class.
+ * </p>
+ *
+ */
+public class ZipResourceExtractor {
+
+    private static final Logger log = LoggerFactory
+            .getLogger(ZipResourceExtractor.class);
+
+    /**
+     * <p>
+     * extract.
+     * </p>
+     *
+     * @param resourcePath a {@link java.lang.String} object.
+     * @param destDir a {@link java.io.File} object.
+     * @return a {@link java.io.File} object.
+     */
+    public static File extract(String resourcePath, File destDir) {
+        log.info("Extracting " + resourcePath + " to directory: " + destDir);
+        if (!destDir.exists()) {
+            destDir.mkdirs();
+        }
+        URL resourceURL = ZipResourceExtractor.class.getResource(resourcePath);
+        if (resourceURL == null) {
+            throw new NullPointerException(
+                    "Could not retrieve resource for path: " + resourcePath);
+        }
+        try (InputStream resourceInputStream = resourceURL.openStream()) {
+            try {
+                String outname = new File(resourceURL.getPath()).getName();
+                outname = outname.replaceAll("%20", " ");
+                log.info(outname);
+                if (resourcePath.endsWith("zip")) {
+                    return extractZipArchive(resourceInputStream, destDir);
+                } else if (resourcePath.endsWith("gz")) {
+                    outname = outname.substring(0, outname.lastIndexOf("."));
+                    return copyToOutputFile(destDir, outname,
+                            new GZIPInputStream(new BufferedInputStream(
+                                    resourceInputStream)));
+                } else {
+                    return copyToOutputFile(destDir, outname,
+                            new BufferedInputStream(resourceInputStream));
+                }
+            } catch (FileNotFoundException e) {
+                log.warn(e.getLocalizedMessage());
+                throw new RuntimeException(e);
+            }
+        } catch (IOException ex) {
+            log.error("Caught IOException: ", ex);
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private static File copyToOutputFile(File destDir, String outname,
+            InputStream in) throws FileNotFoundException, IOException {
+        File outputFile = new File(destDir, outname);
+        try (BufferedOutputStream out = new BufferedOutputStream(
+                new FileOutputStream(outputFile))) {
+            // Transfer bytes from in to out
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+        }
+        return outputFile;
+    }
+
+    /**
+     * <p>
+     * extractZipArchive.
+     * </p>
+     *
+     * @param istream a {@link java.io.InputStream} object.
+     * @param outputDir a {@link java.io.File} object.
+     * @return a {@link java.io.File} object.
+     */
+    public static File extractZipArchive(InputStream istream, File outputDir) {
+        try {
+            File outDir;
+            try (ZipInputStream zis = new ZipInputStream(
+                    new BufferedInputStream(istream))) {
+                ZipEntry entry;
+                outDir = null;
+                while ((entry = zis.getNextEntry()) != null) {
+                    int size;
+                    byte[] buffer = new byte[2048];
+                    File outFile = new File(outputDir, entry.getName());
+                    if (entry.isDirectory()) {
+                        outFile.mkdirs();
+                        if (outDir == null) {
+                            outDir = outFile;
+                        }
+                    } else {
+                        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(
+                                outFile), buffer.length)) {
+                            while ((size = zis.read(buffer, 0, buffer.length)) != -1) {
+                                bos.write(buffer, 0, size);
+                            }
+                            bos.flush();
+                        }
+                    }
+                }
+                if (outDir == null) {
+                    outDir = outputDir;
+                }
+            }
+            istream.close();
+            return outDir;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
