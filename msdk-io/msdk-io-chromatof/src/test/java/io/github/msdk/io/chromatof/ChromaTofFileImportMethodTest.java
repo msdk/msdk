7eeea4e596c1cb9ad6490e610133eb664d8e44e9
nilshoffmann <nhoffmann@netbeans.org>
2016-01-04 21:19:46 +0100

    Proof of concept for migration of Maltcms ChromaTof Parser to MSDK.

--- msdk-io/msdk-io-chromatof/src/test/java/io/github/msdk/io/chromatof/ChromaTofFileImportMethodTest.java
+++ msdk-io/msdk-io-chromatof/src/test/java/io/github/msdk/io/chromatof/ChromaTofFileImportMethodTest.java
@@ -14,8 +14,16 @@
 
 package io.github.msdk.io.chromatof;
 
+import io.github.msdk.MSDKException;
+import io.github.msdk.datamodel.datastore.DataPointStore;
+import io.github.msdk.datamodel.datastore.DataPointStoreFactory;
+import io.github.msdk.datamodel.featuretables.FeatureTable;
+import io.github.msdk.datamodel.featuretables.Sample;
+import java.io.File;
+import java.util.List;
+import java.util.Locale;
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -23,12 +31,36 @@ import static org.junit.Assert.*;
  */
 public class ChromaTofFileImportMethodTest {
     
-    public ChromaTofFileImportMethodTest() {
-    }
+    private static final String TEST_DATA_PATH = "src/test/resources/";
+    
+    @SuppressWarnings("null")
+    @Test
+    public void GCxGC_Import() throws MSDKException {
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+        // Create the data structures
+        DataPointStore dataStore = DataPointStoreFactory.getTmpFileDataStore();
+
+        // Import the file
+        File inputFile = new File(TEST_DATA_PATH + "GGT1.txt");
+        Assert.assertTrue(inputFile.canRead());
+        ChromaTofFileImportMethod importer = new ChromaTofFileImportMethod(inputFile,
+                dataStore, Locale.US, ChromaTofParser.FIELD_SEPARATOR_TAB, ChromaTofParser.QUOTATION_CHARACTER_NONE);
+        FeatureTable featureTable = importer.execute();
+        Assert.assertNotNull(featureTable);
+        Assert.assertEquals(1.0, importer.getFinishedPercentage(), 0.0001);
+
+        // The table has columns
+        Assert.assertFalse(featureTable.getColumns().isEmpty());
+
+        // The table has 1 sample
+        List<Sample> samples = featureTable.getSamples();
+        Assert.assertNotNull(samples);
+        Assert.assertEquals(1, samples.size());
+
+        // The table has 15 features
+        Assert.assertFalse(featureTable.getRows().isEmpty());
+        Assert.assertEquals(15, featureTable.getRows().size());
+
+        featureTable.dispose();
+    }
 }
