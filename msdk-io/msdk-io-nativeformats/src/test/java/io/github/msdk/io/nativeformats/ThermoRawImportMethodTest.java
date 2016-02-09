733a31b24311d43104bf0eac07f81cb0ffdd5ef6
Tomas Pluskal <plusik@gmail.com>
2016-02-09 11:29:42 -0500

    Fixed parsing of precursor m/z for Thermo SRM files

--- msdk-io/msdk-io-nativeformats/src/test/java/io/github/msdk/io/nativeformats/ThermoRawImportMethodTest.java
+++ msdk-io/msdk-io-nativeformats/src/test/java/io/github/msdk/io/nativeformats/ThermoRawImportMethodTest.java
@@ -197,4 +197,54 @@ public class ThermoRawImportMethodTest {
 
     }
 
+    @SuppressWarnings("null")
+    @Test
+    public void testSRM() throws Exception {
+
+        // Run this test only on Windows
+        Assume.assumeTrue(System.getProperty("os.name").startsWith("Windows"));
+
+        // Create the data structures
+        DataPointStore dataStore = DataPointStoreFactory.getMemoryDataStore();
+        double mzBuffer[] = new double[10000];
+        float intensityBuffer[] = new float[10000];
+
+        // Import the file
+        File inputFile = new File(TEST_DATA_PATH + "srm.raw");
+        Assert.assertTrue(inputFile.canRead());
+        ThermoRawImportMethod importer = new ThermoRawImportMethod(inputFile,
+                dataStore);
+        RawDataFile rawFile = importer.execute();
+        Assert.assertNotNull(rawFile);
+        Assert.assertEquals(1.0, importer.getFinishedPercentage(), 0.0001);
+
+        // The file has 1490 scans
+        List<MsScan> scans = rawFile.getScans();
+        Assert.assertNotNull(scans);
+        Assert.assertEquals(1490, scans.size());
+
+        // 10th scan, #10
+        MsScan scan10 = scans.get(9);
+        Assert.assertEquals(new Integer(10), scan10.getScanNumber());
+        Assert.assertEquals(MsSpectrumType.CENTROIDED,
+                scan10.getSpectrumType());
+        Assert.assertEquals(new Integer(2),
+                scan10.getMsFunction().getMsLevel());
+        Assert.assertEquals("srm", scan10.getMsFunction().getName());
+        Assert.assertEquals(165.000,
+                scan10.getIsolations().get(0).getPrecursorMz(), 0.01);
+        Assert.assertEquals(9.293f,
+                scan10.getChromatographyInfo().getRetentionTime(), 0.01f);
+        Assert.assertEquals(PolarityType.POSITIVE, scan10.getPolarity());
+        mzBuffer = scan10.getMzValues(mzBuffer);
+        intensityBuffer = scan10.getIntensityValues(intensityBuffer);
+        Float scan10maxInt = MsSpectrumUtil.getMaxIntensity(intensityBuffer,
+                scan10.getNumberOfDataPoints());
+        Assert.assertEquals(2.05E4f, scan10maxInt, 1E2f);
+        Double scan10basePeakMz = mzBuffer[MsSpectrumUtil.getBasePeakIndex(
+                intensityBuffer, scan10.getNumberOfDataPoints())];
+        Assert.assertEquals(119.1188, scan10basePeakMz, 0.0001);
+
+    }
+
 }
