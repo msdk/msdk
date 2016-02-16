0a754fb555d5148b01f1faf283d1ff818c7fb581
Tomas Pluskal <plusik@gmail.com>
2016-02-16 15:07:57 -0500

    Fixed a bug in txt parser

---
+++ msdk-datamodel/src/main/java/io/github/msdk/util/ArrayUtil.java
@@ -0,0 +1,52 @@
+/* 
+ * (C) Copyright 2015-2016 by MSDK Development Team
+ *
+ * This software is dual-licensed under either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+
+package io.github.msdk.util;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Array utilities
+ */
+public class ArrayUtil {
+
+    public static @Nonnull double[] addToArray(@Nonnull double[] array,
+            double value, int pos) {
+
+        if (pos >= array.length) {
+            double newArray[] = new double[pos * 2];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            array = newArray;
+        }
+
+        array[pos] = value;
+        return array;
+
+    }
+
+    public static @Nonnull float[] addToArray(@Nonnull float[] array,
+            float value, int pos) {
+
+        if (pos >= array.length) {
+            float newArray[] = new float[pos * 2];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            array = newArray;
+        }
+
+        array[pos] = value;
+        return array;
+
+    }
+
+}
