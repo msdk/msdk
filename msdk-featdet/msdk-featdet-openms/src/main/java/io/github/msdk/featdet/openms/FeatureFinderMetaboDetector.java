2ff88eaa1a7f763af225ec0a3f30b75ff36c0fc5
Adhithya <akashr1997@gmail.com>
2017-03-21 21:51:23 +0530

    Required fixes after Code Review

---
+++ msdk-featdet/msdk-featdet-openms/src/main/java/io/github/msdk/featdet/openms/FeatureFinderMetaboDetector.java
@@ -0,0 +1,108 @@
+/* 
+ * (C) Copyright 2015-2016 by MSDK Development Team
+ *
+ * This software is dual-licensed under either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+
+package io.github.msdk.featdet.openms;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class FeatureFinderMetaboDetector {
+	private final String OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME;
+	private final File FEATURE_XML_FILE;
+
+	private final @Nonnull File mzMLFile;
+
+	public FeatureFinderMetaboDetector(String mzMLFileName) throws FeatureFinderMetaboException {
+		this(new File(mzMLFileName));
+	}
+
+	public FeatureFinderMetaboDetector(File mzMLFile) throws FeatureFinderMetaboException {
+		this.mzMLFile = mzMLFile;
+		FEATURE_XML_FILE = new File(mzMLFile.getName() + "featureXML");
+		OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME = FeatureFinderMetaboLocator.findFeatureFinderMetabo();
+	}
+
+	public List<Double> execute()
+			throws SAXException, IOException, ParserConfigurationException, FeatureFinderMetaboException {
+		List<Double> mzValuesResult = new ArrayList<Double>();
+
+		if (!OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME.equals(null)) {
+			createTempFeatureXMLFile();
+
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(FEATURE_XML_FILE);
+			doc.getDocumentElement().normalize();
+
+			NodeList featureMap = doc.getElementsByTagName("featureMap").item(0).getChildNodes();
+			NodeList featureList = null;
+			for (int i = 0; i < featureMap.getLength(); i++) {
+				Node node = featureMap.item(i);
+				if (node.getNodeName().equals("featureList"))
+					featureList = node.getChildNodes();
+			}
+
+			for (int i = 0; i < featureList.getLength(); i++) {
+				Node node = featureList.item(i);
+				if (node.getNodeName().equals("feature")) {
+					Element featureElement = (Element) node;
+					mzValuesResult.add(
+							Double.valueOf(featureElement.getElementsByTagName("position").item(1).getTextContent()));
+				}
+			}
+		} else {
+			throw new FeatureFinderMetaboException(
+					"FeatureFinderMetabo library not found. Please install OpenMS and add FeatureFinderMetabo to Path.");
+		}
+		return mzValuesResult;
+
+	}
+
+	private void createTempFeatureXMLFile() throws IOException, FeatureFinderMetaboException {
+		final Logger logger = LoggerFactory.getLogger(this.getClass());
+		logger.info(execShellCommand(OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME + " -in " + mzMLFile.getAbsolutePath()
+				+ " -out " + FEATURE_XML_FILE.getAbsolutePath()));
+		if (!FEATURE_XML_FILE.exists())
+			throw new FeatureFinderMetaboException("Could not create featureXML File");
+		FEATURE_XML_FILE.deleteOnExit();
+	}
+
+	private String execShellCommand(String cmd) throws IOException {
+		String out = "";
+		Process cmdProc = Runtime.getRuntime().exec(cmd);
+		BufferedReader stdoutReader = new BufferedReader(new InputStreamReader(cmdProc.getInputStream()));
+		String line;
+		while ((line = stdoutReader.readLine()) != null) {
+			out += line + "\n";
+		}
+		return out;
+	}
+}
---
+++ msdk-featdet/msdk-featdet-openms/src/main/java/io/github/msdk/featdet/openms/FeatureFinderMetaboLocator.java
@@ -0,0 +1,92 @@
+/* 
+ * (C) Copyright 2015-2016 by MSDK Development Team
+ *
+ * This software is dual-licensed under either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+
+package io.github.msdk.featdet.openms;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FeatureFinderMetaboLocator {
+
+	private final static String OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME = "FeatureFinderMetabo";
+	private final static String OPENMS_DEFAULT_LOCATION_WINDOWS = "C:\\Program Files\\";
+	private final static String OPENMS_DEFAULT_LOCATION_MAC = "/Applications/";
+	private final static String OPENMS_DEFAULT_LOCATION_LINUX = "/usr/local/";
+	private final static String FEATURE_FINDER_METABO_EVAL = "No options given. Aborting!";
+
+	public static String findFeatureFinderMetabo() throws FeatureFinderMetaboException {
+		if (isFeatureFinderMetaboHere(OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME)) {
+			return OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME;
+		} else {
+			String OS = System.getProperty("os.name").toLowerCase();
+			if (OS.indexOf("win") >= 0) {
+				for (String s : new File(OPENMS_DEFAULT_LOCATION_WINDOWS).list()) {
+					if (s.toLowerCase().contains("openms")) {
+						String path = OPENMS_DEFAULT_LOCATION_WINDOWS + s + "\\bin\\"
+								+ OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME;
+						if (isFeatureFinderMetaboHere(path))
+							return path;
+						return null;
+					}
+				}
+			} else if (OS.indexOf("mac") >= 0) {
+				for (String s : new File(OPENMS_DEFAULT_LOCATION_MAC).list()) {
+					if (s.toLowerCase().contains("openms")) {
+						String path = OPENMS_DEFAULT_LOCATION_MAC + s + "\\bin\\"
+								+ OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME;
+						if (isFeatureFinderMetaboHere(path))
+							return path;
+						return null;
+					}
+				}
+			} else if (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0) {
+				for (String s : new File(OPENMS_DEFAULT_LOCATION_LINUX).list()) {
+					if (s.toLowerCase().contains("openms")) {
+						String path = OPENMS_DEFAULT_LOCATION_LINUX + s + "\\bin\\"
+								+ OPENMS_FEATURE_FINDER_METABO_LIBRARY_NAME;
+						if (isFeatureFinderMetaboHere(path))
+							return path;
+						return null;
+					}
+
+				}
+			} else {
+				return null;
+			}
+		}
+		return null;
+	}
+
+	private static boolean isFeatureFinderMetaboHere(String path) throws FeatureFinderMetaboException {
+		return execShellCommand(path).contains(FEATURE_FINDER_METABO_EVAL);
+	}
+
+	private static String execShellCommand(String cmd) throws FeatureFinderMetaboException {
+		String out = "";
+		try {
+			Process cmdProc = Runtime.getRuntime().exec(cmd);
+			BufferedReader stdoutReader = new BufferedReader(new InputStreamReader(cmdProc.getInputStream()));
+			String line;
+			while ((line = stdoutReader.readLine()) != null) {
+				out += line + "\n";
+			}
+		} catch (IOException ie) {
+			throw new FeatureFinderMetaboException(ie.getMessage());
+		}
+		return out;
+	}
+
+}
