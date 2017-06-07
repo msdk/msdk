c9731cc14b4caa6b59a3ea11a7554978bd75bf18
Adhithya <akashr1997@gmail.com>
2017-06-07 20:25:53 +0530

    Implementing code from MSFTBX for decoding binary data

---
+++ msdk-io/msdk-io-mzml2/src/main/java/io/github/msdk/io/mzml2/util/ByteArrayHolderFactory.java
@@ -0,0 +1,81 @@
+/* 
+ * Copyright 2016 Dmitry Avtonomov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.msdk.io.mzml2.util;
+
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+
+/**
+ * @author Dmitry Avtonomov
+ */
+public class ByteArrayHolderFactory extends BasePooledObjectFactory<ByteArrayHolder> {
+    protected volatile int defaultSize = 8192; // default to 8192
+
+    public ByteArrayHolderFactory() {
+    }
+
+    public ByteArrayHolderFactory(int defaultSize) {
+        this.defaultSize = defaultSize;
+    }
+
+    public int getDefaultSize() {
+        return defaultSize;
+    }
+
+    /**
+     * Sets the default size of byte buffers, produced by this factory.
+     * @param defaultSize new default size
+     */
+    public synchronized void setDefaultSize(int defaultSize) {
+        this.defaultSize = defaultSize;
+    }
+
+    /**
+     * Updates the default byte buffer size, if the new value is larger, than
+     * the current default.
+     * @param defaultSize new default size
+     */
+    public synchronized void setDefaultSizeMax(int defaultSize) {
+        if (this.defaultSize < defaultSize) {
+            this.defaultSize = defaultSize;
+        }
+    }
+
+    @Override
+    public ByteArrayHolder create() throws Exception {
+        return new ByteArrayHolder(getDefaultSize());
+    }
+
+    @Override
+    public PooledObject<ByteArrayHolder> wrap(ByteArrayHolder obj) {
+        return new DefaultPooledObject<>(obj);
+    }
+
+    @Override
+    public void passivateObject(PooledObject<ByteArrayHolder> p) throws Exception {
+        ByteArrayHolder bah = p.getObject();
+        if (bah == null) return;
+        bah.reset();
+    }
+
+    @Override
+    public void activateObject(PooledObject<ByteArrayHolder> p) throws Exception {
+        ByteArrayHolder bah = p.getObject();
+        if (bah == null) return;
+        bah.reset();
+    }
+}
---
+++ msdk-io/msdk-io-mzml2/src/main/java/io/github/msdk/io/mzml2/util/IntDecoder.java
@@ -0,0 +1,76 @@
+/*
+	IntDecoder.java
+	johan.teleman@immun.lth.se
+
+	Copyright 2013 Johan Teleman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package io.github.msdk.io.mzml2.util;
+
+/**
+ * Decodes ints from the half bytes in bytes. Lossless reverse of encodeInt,
+ * although not symmetrical in input arguments.
+ */
+class IntDecoder {
+
+    int pos = 0;
+    boolean half = false;
+    byte[] bytes;
+
+    public IntDecoder(byte[] _bytes, int _pos) {
+        bytes = _bytes;
+        pos = _pos;
+    }
+
+    public long next() {
+        int head;
+        int i, n;
+        long res = 0;
+        long mask, m;
+        int hb;
+
+        if (!half)
+            head = (0xff & bytes[pos]) >> 4;
+        else
+            head = 0xf & bytes[pos++];
+
+        half = !half;
+
+        if (head <= 8)
+            n = head;
+        else {
+            // leading ones, fill in res
+            n = head - 8;
+            mask = 0xf0000000;
+            for (i = 0; i < n; i++) {
+                m = mask >> (4 * i);
+                res = res | m;
+            }
+        }
+
+        if (n == 8) return 0;
+
+        for (i = n; i < 8; i++) {
+            if (!half)
+                hb = (0xff & bytes[pos]) >> 4;
+            else
+                hb = 0xf & bytes[pos++];
+
+            res = res | (hb << ((i - n) * 4));
+            half = !half;
+        }
+
+        return res;
+    }
+}
