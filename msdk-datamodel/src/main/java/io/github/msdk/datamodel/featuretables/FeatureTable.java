395da076d0a9415f47353217d4ae25a4b05b0ff7
Tomas Pluskal <plusik@gmail.com>
2017-06-18 11:15:22 -0400

    Changed feature table interfaces

--- msdk-datamodel/src/main/java/io/github/msdk/datamodel/featuretables/FeatureTable.java
+++ msdk-datamodel/src/main/java/io/github/msdk/datamodel/featuretables/FeatureTable.java
@@ -15,107 +15,25 @@ package io.github.msdk.datamodel.featuretables;
 import java.util.List;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.annotation.concurrent.NotThreadSafe;
-
-import com.google.common.collect.Range;
 
 /**
  * A feature table consists of a list of named columns, each with a specific type for the values
  * contained in it.
- *
  */
 @NotThreadSafe
 public interface FeatureTable {
 
   /**
-   * <p>
-   * getName.
-   * </p>
-   *
-   * @return Short descriptive name for the feature table
-   */
-  @Nonnull
-  String getName();
-
-  /**
-   * Change the name of this feature table.
-   *
-   * @param name the name of the feature table.
-   */
-  void setName(@Nonnull String name);
-
-  /**
-   * Returns an immutable list of columns
-   *
-   * @return the list of feature table columns.
-   */
-  @Nonnull
-  List<FeatureTableColumn<?>> getColumns();
-
-  /**
-   * Returns a specific column for the given {@code columnName}. May return {@code null} if no
-   * column for the given name is known.
-   *
-   * @param <DATATYPE> the generic datatype of the column's entries.
-   * @param columnName the name of the column.
-   * @param sample the {@link io.github.msdk.datamodel.featuretables.Sample}.
-   * @param dtType the class of the column' datatype.
-   * @return the feature table column for the given name, or null.
-   */
-  @Nullable
-  <DATATYPE> FeatureTableColumn<DATATYPE> getColumn(@Nonnull String columnName, Sample sample,
-      Class<? extends DATATYPE> dtType);
-
-  /**
-   * Returns a specific column for the given name.
-   *
-   * @param <DATATYPE> the generic datatype of the column's entries.
-   * @param columnName the {@link io.github.msdk.datamodel.featuretables.ColumnName} of the column.
-   * @param sample the {@link io.github.msdk.datamodel.featuretables.Sample}.
-   * @return the {@link io.github.msdk.datamodel.featuretables.FeatureTableColumn} for the given name and sample.
-   */
-  @Nullable
-  <DATATYPE> FeatureTableColumn<DATATYPE> getColumn(@Nonnull ColumnName columnName, Sample sample);
-
-  /**
-   * Add a new column to the feature table.
-   *
-   * @param col the {@link io.github.msdk.datamodel.featuretables.FeatureTableColumn} to add.
-   */
-  void addColumn(@Nonnull FeatureTableColumn<?> col);
-
-  /**
-   * Removes a column from this feature table.
-   *
-   * @param col the {@link io.github.msdk.datamodel.featuretables.FeatureTableColumn} to remove.
-   */
-  void removeColumn(@Nonnull FeatureTableColumn<?> col);
-
-  /**
    * Returns an immutable list of rows
    *
-   * @return a list of {@link io.github.msdk.datamodel.featuretables.FeatureTableRow}s.
+   * @return a list of {@link FeatureTableRow}s.
    */
   @Nonnull
   List<FeatureTableRow> getRows();
 
   /**
-   * Add a new row to the feature table.
-   *
-   * @param row the {@link io.github.msdk.datamodel.featuretables.FeatureTableRow} to add.
-   */
-  void addRow(@Nonnull FeatureTableRow row);
-
-  /**
-   * Removes a row from this feature table.
-   *
-   * @param row the {@link io.github.msdk.datamodel.featuretables.FeatureTableRow} to remove.
-   */
-  void removeRow(@Nonnull FeatureTableRow row);
-
-  /**
-   * Shortcut to return an immutable list of {@link io.github.msdk.datamodel.featuretables.Sample}s found in this feature table.
+   * Shortcut to return an immutable list of {@link Sample}s found in this feature table.
    *
    * @return the list of samples.
    */
@@ -126,15 +44,5 @@ public interface FeatureTable {
    * Remove all data associated to this feature table.
    */
   void dispose();
-
-  /**
-   * Shortcut to return an immutable list of {@link io.github.msdk.datamodel.featuretables.FeatureTableRow}s found in this feature table
-   * with average m/z and RT values within the given ranges.
-   *
-   * @return the list of feature table rows.
-   * @param rtRange a {@link com.google.common.collect.Range} object.
-   * @param mzRange a {@link com.google.common.collect.Range} object.
-   */
-  List<FeatureTableRow> getRowsInsideRange(Range<Float> rtRange, Range<Double> mzRange);
 
 }
