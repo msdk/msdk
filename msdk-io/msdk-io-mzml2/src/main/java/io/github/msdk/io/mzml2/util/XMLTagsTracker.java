1be7c6ba0d8033486406dae140f7464fb36d4788
Adhithya <akashr1997@gmail.com>
2017-06-23 02:49:55 +0530

    Use Stacks for tracking tags instead of path string

--- msdk-io/msdk-io-mzml2/src/main/java/io/github/msdk/io/mzml2/util/XMLTagsTracker.java
+++ msdk-io/msdk-io-mzml2/src/main/java/io/github/msdk/io/mzml2/util/XMLTagsTracker.java
@@ -1,36 +1,30 @@
 package io.github.msdk.io.mzml2.util;
 
+import java.util.Stack;
+
 import io.github.msdk.MSDKRuntimeException;
 
 public class XMLTagsTracker {
 
-  private String path;
+  private Stack<String> tagStack;
 
   public XMLTagsTracker() {
-    path = "";
+    tagStack = new Stack<>();
   }
 
-  public String enteredTag(String tag) {
-    path += "/" + tag;
-    return path;
+  public void enteredTag(String tag) {
+    tagStack.push(tag);
   }
 
-  public String exitedTag(String tag) {
-    String exitTag = path.substring(path.lastIndexOf('/') + 1, path.length());
-    if (!tag.equals(exitTag))
+  public void exitedTag(String tag) {
+    if (!tagStack.peek().equals(tag))
       throw (new MSDKRuntimeException(
-          "Cannot exit tag " + tag + "\nLast tag entered was " + exitTag));
-    path = path.substring(0, path.lastIndexOf('/'));
-    return path;
-  }
-
-  public String getPath() {
-    return path;
+          "Cannot exit tag " + tag + "\nLast tag entered was " + tagStack.peek()));
+    tagStack.pop();
   }
 
   public boolean isInside(String tag) {
-    String exitTag = path.substring(path.lastIndexOf('/') + 1, path.length());
-    if (tag.equals(exitTag) || path.contains("/" + tag + "/")) {
+    if (tagStack.contains(tag)) {
       return true;
     }
     return false;
