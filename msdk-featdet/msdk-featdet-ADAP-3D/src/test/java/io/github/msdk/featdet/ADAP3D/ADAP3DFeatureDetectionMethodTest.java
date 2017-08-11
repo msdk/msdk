32bd2d0d215a69e751f17c4f20ae55ea15ad28c5
dharak029 <dshah27@uncc.edu>
2017-08-11 14:26:28 -0400

    unused reference removed.

--- msdk-featdet/msdk-featdet-ADAP-3D/src/test/java/io/github/msdk/featdet/ADAP3D/ADAP3DFeatureDetectionMethodTest.java
+++ msdk-featdet/msdk-featdet-ADAP-3D/src/test/java/io/github/msdk/featdet/ADAP3D/ADAP3DFeatureDetectionMethodTest.java
@@ -14,7 +14,6 @@ package io.github.msdk.featdet.ADAP3D;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
