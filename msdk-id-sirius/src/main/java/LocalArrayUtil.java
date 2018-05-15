20f8c5887c9df712ff018836a6584ce20b70c9de
Evgeny Sorokin <evgerher1l@gmail.com>
2018-05-15 23:44:15 +0300

    Trivial float[] to double[] caster

--- msdk-id-sirius/src/main/java/LocalArrayUtil.java
+++ msdk-id-sirius/src/main/java/LocalArrayUtil.java
@@ -3,7 +3,7 @@ import javax.annotation.Nonnull;
 /**
  * Created by evger on 15-May-18.
  */
-public class ArrayUtil {
+public class LocalArrayUtil {
 
   public static @Nonnull double[] convertToDoubles(@Nonnull float[] array) {
     double doubles[] = new double[array.length];
