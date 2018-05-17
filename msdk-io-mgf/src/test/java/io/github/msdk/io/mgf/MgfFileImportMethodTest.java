54a3abf5d4ab6c758ecca68b0a13236848898b6a
Evgeny Sorokin <evgerher1l@gmail.com>
2018-05-17 19:18:59 +0300

    Test asserts

--- msdk-io-mgf/src/test/java/io/github/msdk/io/mgf/MgfFileImportMethodTest.java
+++ msdk-io-mgf/src/test/java/io/github/msdk/io/mgf/MgfFileImportMethodTest.java
@@ -3,6 +3,7 @@ package io.github.msdk.io.mgf;
 import io.github.msdk.MSDKException;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.junit.Assert;
@@ -14,11 +15,21 @@ import org.junit.Test;
 public class MgfFileImportMethodTest {
   @Test
   public void testMgfImport() throws IOException, MSDKException {
+    final int expectedSize = 1;
+    final String expectedTitle = "example.9.9.2";
+    final long expectedNumberDatapoints = 18;
+    final int expectedCharge = 2;
+
     File file = new File("target/test-classes/test_query.mgf");
     MgfFileImportMethod importMethod = new MgfFileImportMethod(file);
-    importMethod.execute();
+    Collection<MgfMsSpectrum> spectrums = importMethod.execute();
+    int size = spectrums.size();
+    MgfMsSpectrum spectrum = (MgfMsSpectrum)spectrums.toArray()[0];
 
-    Assert.assertEquals(true, true);
+    Assert.assertEquals(expectedSize, size);
+    Assert.assertEquals(expectedTitle, spectrum.getTitle());
+    Assert.assertEquals(Long.valueOf(expectedNumberDatapoints), Long.valueOf(spectrum.getNumberOfDataPoints()));
+    Assert.assertEquals(expectedCharge, spectrum.getPrecursorCharge());
   }
 
 }
