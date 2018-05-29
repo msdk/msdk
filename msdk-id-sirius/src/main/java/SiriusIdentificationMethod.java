d9168cc268c56d470c6c4210da59b591b8af3deb
Evgeny Sorokin <evgerher1l@gmail.com>
2018-05-29 20:41:13 +0300

    License header

--- msdk-id-sirius/src/main/java/SiriusIdentificationMethod.java
+++ msdk-id-sirius/src/main/java/SiriusIdentificationMethod.java
@@ -1,3 +1,16 @@
+/*
+ * (C) Copyright 2015-2017 by MSDK Development Team
+ *
+ * This software is dual-licensed under either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1 as published by the Free
+ * Software Foundation
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by the Eclipse Foundation.
+ */
+
 import de.unijena.bioinf.ChemistryBase.chem.PrecursorIonType;
 import de.unijena.bioinf.ChemistryBase.ms.Ms2Experiment;
 import de.unijena.bioinf.ChemistryBase.ms.Peak;
