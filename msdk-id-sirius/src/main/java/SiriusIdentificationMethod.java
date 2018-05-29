c5b8466a811bcc2e61ae51a2dbaaa7cda128c542
Evgeny Sorokin <evgerher1l@gmail.com>
2018-05-29 22:16:06 +0300

    Refactor sirius wrapper class - separate sirius and msdk blocks

--- msdk-id-sirius/src/main/java/SiriusIdentificationMethod.java
+++ msdk-id-sirius/src/main/java/SiriusIdentificationMethod.java
@@ -39,20 +39,39 @@ import javax.annotation.Nullable;
  */
 
 public class SiriusIdentificationMethod implements MSDKMethod<List<IonAnnotation>> {
+
   private final Sirius sirius;
   private MsSpectrum ms1;
   private MsSpectrum ms2;
   private List<IonAnnotation> result;
+  private double parentMass;
+  private String adduct;
+  private int numberOfCandidates;
 
-  public SiriusIdentificationMethod() {
+  public SiriusIdentificationMethod(@Nullable MsSpectrum ms1, MsSpectrum ms2, double parentMass,
+      String ion) {
     sirius = new Sirius();
+    this.ms1 = ms1;
+    this.ms2 = ms2;
+    this.parentMass = parentMass;
+    this.adduct = ion;
+    numberOfCandidates = 5;
+  }
+
+  public void setNumberOfCandidates(int number) {
+    numberOfCandidates = number;
+  }
+
+  public int getNumberOfCandidates() {
+    return numberOfCandidates;
   }
 
   /**
    * This function is left here for custom spectrum files (just columns of mz and intensity values)
    * Does similar to mgf parser functionality
-   * */
-  public MsSpectrum readCustomMsFile(String path, String delimeter) throws IOException, MSDKRuntimeException {
+   */
+  public static MsSpectrum readCustomMsFile(String path, String delimeter)
+      throws IOException, MSDKRuntimeException {
     Scanner sc = new Scanner(new File(path));
     ArrayList<String> strings = new ArrayList<>();
     while (sc.hasNext()) {
@@ -70,27 +89,34 @@ public class SiriusIdentificationMethod implements MSDKMethod<List<IonAnnotation
       if (splitted.length == 2) {
         mz[index] = Double.parseDouble(splitted[0]);
         intensity[index++] = Float.parseFloat(splitted[1]);
-      } else throw new MSDKRuntimeException("Incorrect spectrum structure");
+      } else {
+        throw new MSDKRuntimeException("Incorrect spectrum structure");
+      }
     }
 
     MsSpectrumType type = SpectrumTypeDetectionAlgorithm.detectSpectrumType(mz, intensity, size);
     return new SimpleMsSpectrum(mz, intensity, size, type);
   }
 
-  private List<IdentificationResult> siriusProcessSpectrums(
-      @Nullable MsSpectrum inputMs1, MsSpectrum inputMs2, double parentMass,
-      String ion, int numberOfCandidates) throws MSDKException, IOException {
 
+  /**
+   * Transformation of MSDK data structures into Sirius structures
+   */
+  private List<IdentificationResult> siriusProcessSpectrums() throws MSDKException {
     Spectrum<Peak> siriusMs1 = null, siriusMs2;
-    siriusMs2 = sirius.wrapSpectrum(inputMs2.getMzValues(), LocalArrayUtil.convertToDoubles(inputMs2.getIntensityValues()));
-    if (siriusMs1 != null) {
-      siriusMs1 = sirius.wrapSpectrum(inputMs1.getMzValues(), LocalArrayUtil.convertToDoubles(inputMs1.getIntensityValues()));
+    siriusMs2 = sirius
+        .wrapSpectrum(ms2.getMzValues(), LocalArrayUtil.convertToDoubles(ms2.getIntensityValues()));
+    if (ms1 != null) {
+      siriusMs1 = sirius.wrapSpectrum(ms1.getMzValues(),
+          LocalArrayUtil.convertToDoubles(ms1.getIntensityValues()));
     }
 
-    PrecursorIonType precursor = sirius.getPrecursorIonType(ion);
+    PrecursorIonType precursor = sirius.getPrecursorIonType(adduct);
     Ms2Experiment experiment = sirius.getMs2Experiment(parentMass, precursor, siriusMs1, siriusMs2);
 
-    List<IdentificationResult> siriusResults = sirius.identify(experiment, numberOfCandidates, true, IsotopePatternHandling.omit);
+//    TODO: think about IsotopePatternHandling type
+    List<IdentificationResult> siriusResults = sirius
+        .identify(experiment, numberOfCandidates, true, IsotopePatternHandling.omit);
     return siriusResults;
   }
 
@@ -104,7 +130,6 @@ public class SiriusIdentificationMethod implements MSDKMethod<List<IonAnnotation
   @Override
   public List<IonAnnotation> execute() throws MSDKException {
     result = new ArrayList<>();
-
     List<IdentificationResult> siriusSpectrums = siriusProcessSpectrums();
 
     return result;
