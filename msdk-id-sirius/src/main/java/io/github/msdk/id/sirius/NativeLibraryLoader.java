80c9b3ce39a108d2bc6c7fb4a71622361b47369a
Evgeny Sorokin <evgerher1l@gmail.com>
2018-06-19 20:22:32 +0300

    Add interoperability between IDE & JAR loads

--- msdk-id-sirius/src/main/java/io/github/msdk/id/sirius/NativeLibraryLoader.java
+++ msdk-id-sirius/src/main/java/io/github/msdk/id/sirius/NativeLibraryLoader.java
@@ -16,6 +16,7 @@ package io.github.msdk.id.sirius;
 import com.google.common.io.Files;
 import io.github.msdk.MSDKException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -32,7 +33,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import org.slf4j.Logger;
@@ -40,10 +40,9 @@ import org.slf4j.LoggerFactory;
 
 /**
  * <p> Class NativeLibraryLoader </p> This class allows to dynamically load native libraries from
- * .jar files with updating java.library.path variable
+ * .jar files (also works with IDE) with updating java.library.path variable
  */
 public class NativeLibraryLoader {
-
   private static final Logger logger = LoggerFactory.getLogger(NativeLibraryLoader.class);
   private NativeLibraryLoader() {}
 
@@ -60,10 +59,6 @@ public class NativeLibraryLoader {
     } catch (URISyntaxException e) {
       throw new MSDKException(e);
     }
-  }
-
-  private static InputStream getResourceStream(String resource) {
-    return NativeLibraryLoader.class.getResourceAsStream(resource);
   }
 
   /**
@@ -93,7 +88,6 @@ public class NativeLibraryLoader {
       throws MSDKException, IOException {
     logger.info("Started loading libraries from {} folder", folder);
 
-    String realPath;
     String arch = getArch();
     String osname = getOsName();
     String subfolder = getSubfolder(arch, osname);
@@ -106,43 +100,22 @@ public class NativeLibraryLoader {
     folder += "/" + subfolder + arch + "/";
 
     final File jarFile = new File(NativeLibraryLoader.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-    final Map<String, InputStream> libraries = new TreeMap<>();
+    Map<String, InputStream> libraries;
 
     if(jarFile.isFile()) {  // Run with JAR file
-      Set<JarEntry> entriesSet = new HashSet<>();
-
+      logger.info("Loading libraries from JAR");
       final JarFile jar = new JarFile(jarFile);
-      String name;
-      JarEntry entry;
-
-      final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
-      while(entries.hasMoreElements()) {
-        entry = entries.nextElement();
-        name = entry.getName();
-        if (name.contains(folder)) { //filter according to the path
-          do {
-            if (!entry.isDirectory()) entriesSet.add(entry);
-            entry = entries.nextElement();
-            name = entry.getName();
-          } while (name.contains(folder));
-          break;
-        }
-      }
-
-      for (final JarEntry libFile: entriesSet) {
-        String filename = libFile.getName();
-        filename = filename.substring(filename.lastIndexOf('/') + 1);
-        InputStream iStream = jar.getInputStream(libFile);
-        libraries.put(filename, iStream);
-      }
-
-      moveLibraries(libraries, temporaryDir, libs);
+      libraries = getJarStreams(jar, folder);
+      moveLibraries(libraries, temporaryDir);
       jar.close();
     } else { // Run with IDE
-      realPath = getResourcePath(folder).toString();
-      // Load native libraries
-      moveLibraries(temporaryDir, realPath, libs);
+      logger.info("Loading libraries from IDE level");
+      String realPath = getResourcePath(folder).toString();
+      libraries = getIdeStreams(realPath);
+      moveLibraries(libraries, temporaryDir);
     }
+
+    loadLibraries(libs);
   }
 
   /**
@@ -197,38 +170,16 @@ public class NativeLibraryLoader {
     usrPathsField.set(null, newPaths);
   }
 
-
   /**
-   * <p>Method moves library from .jar to temporary folder and loads it</p>
+   * * <p>Method moves library from .jar to temporary folder and loads it</p>
    * Temp folder previously should be added to java.library.path
    *
-   * @param tempDirectory - directory to store file at
-   * @param realFolder - directory to copy file from
-   * @throws IOException if any
-   * @throws MSDKException if any
+   * @param fileStreams - map of <filename, inputStream> pairs, used for interoperability between loading from JAR and loading from IDE
+   * @param tempDirectory - temp directory for libraries
+   * @throws FileNotFoundException - in case of troubleshoot with temporary folder.
+   * @throws IOException - in case of incorrect InputStreams or inproper `temp` file created
    */
-  private static void moveLibraries(File tempDirectory, String realFolder, String[] libs)
-      throws IOException, MSDKException {
-
-    File[] files = (new File(realFolder)).listFiles();
-    File temp;
-
-    if (!tempDirectory.exists()) {
-      throw new FileNotFoundException("File " + tempDirectory.getAbsolutePath() + " does not exist");
-    } else {
-      // Copy files
-      for (File f: files) {
-        if (!f.isDirectory()) {
-          temp = new File(tempDirectory, f.getName());
-          Files.copy(f, temp);
-        }
-      }
-
-      loadLibraries(libs);
-    }
-  }
-
-  private static void moveLibraries(Map<String, InputStream> fileStreams, File tempDirectory, String[] libNames) throws FileNotFoundException, IOException {
+  private static void moveLibraries(Map<String, InputStream> fileStreams, File tempDirectory) throws FileNotFoundException, IOException {
     if (tempDirectory == null || !tempDirectory.exists())
       throw new FileNotFoundException("Temporary directory was not created");
 
@@ -237,6 +188,7 @@ public class NativeLibraryLoader {
     int bytes;
 
     for (Map.Entry<String, InputStream> pair: fileStreams.entrySet()) {
+      logger.debug("Started copying {}", pair.getKey());
       File temp = new File(tempDirectory, pair.getKey());
       final InputStream iStream = pair.getValue();
       outputStream = new FileOutputStream(temp);
@@ -246,11 +198,15 @@ public class NativeLibraryLoader {
       }
       iStream.close();
       outputStream.close();
-    }
 
-    loadLibraries(libNames);
+      logger.debug("Finished copying {}", pair.getKey());
+    }
   }
 
+  /**
+   * <p> Method for calling OS to load native libraries stored in java.library.path </p>
+   * @param libraryNames
+   */
   private static void loadLibraries(String[] libraryNames) {
     for (String lib: libraryNames) {
       System.loadLibrary(lib);
@@ -258,6 +214,13 @@ public class NativeLibraryLoader {
     }
   }
 
+  /**
+   * <p>Method returns proper subfolder of a native library</p>
+   * @param arch - architecture of the computer
+   * @param osname - name of the OS (formatted)
+   * @return appropriate folder with native libraries
+   * @throws MSDKException - if system parameters were not determined
+   */
   private static String getSubfolder(String arch, String osname) throws MSDKException {
     String subfolder;
     if (osname.contains("linux")) {
@@ -274,6 +237,11 @@ public class NativeLibraryLoader {
     return subfolder;
   }
 
+  /**
+   * <p>Method returns architecture of the computer</p>
+   * @return formatted architecture
+   * @throws MSDKException - if any
+   */
   private static String getArch() throws  MSDKException {
     String arch = System.getProperty("os.arch");
     if (arch == null)
@@ -282,11 +250,78 @@ public class NativeLibraryLoader {
     return arch.toLowerCase().endsWith("64") ? "64" : "32";
   }
 
+  /**
+   * <p>Method returns formatted OS name</p>
+   * @return OS name
+   * @throws MSDKException - if any
+   */
   private static String getOsName() throws MSDKException {
     String osname = System.getProperty("os.name");
     if (osname == null)
       throw new MSDKException("Can not identify os.name property");
 
     return osname.toLowerCase();
+  }
+
+  /**
+   * <p>Method loads files inside jar from specified folder</p>
+   * @param jar\ - JarFile object
+   * @param folder - appropriate resource folder (i.e. /glpk-4.60/mac64/)
+   * @return map of Filenames & InputStreams
+   * @throws IOException if jarFile was specified wrong
+   */
+  private static Map<String,InputStream> getJarStreams(JarFile jar, String folder) throws IOException {
+    TreeMap<String, InputStream> jarStreams = new TreeMap<>();
+    Set<JarEntry> entriesSet = new HashSet<>();
+
+    String name;
+    JarEntry entry;
+
+    /* Get ALL entries in .jar file */
+    final Enumeration<JarEntry> entries = jar.entries();
+    while(entries.hasMoreElements()) {
+      entry = entries.nextElement();
+      name = entry.getName();
+      /* Find the folder with required pattern i.e. /glpk-4.60/mac64/ */
+      if (name.contains(folder)) {
+
+        /* Process ONLY found directory and ignore other entries*/
+        do {
+          if (!entry.isDirectory()) entriesSet.add(entry);
+          entry = entries.nextElement();
+          name = entry.getName();
+        } while (name.contains(folder));
+
+        break; // Finish processing entries
+      }
+    }
+
+    /* Transform JarEntries into pairs of Filename & InputStream to that file */
+    for (final JarEntry libFile: entriesSet) {
+      String filename = libFile.getName();
+      filename = filename.substring(filename.lastIndexOf('/') + 1);
+      InputStream iStream = jar.getInputStream(libFile);
+      jarStreams.put(filename, iStream);
+    }
+
+    return jarStreams;
+  }
+
+  /**
+   * <p>Method loads files from specified folder </p>
+   * @param folder - folder with native libraries
+   * @return map of Filenames & InputStreams
+   * @throws FileNotFoundException - if any
+   */
+  private static Map<String,InputStream> getIdeStreams(String folder) throws FileNotFoundException {
+    Map<String, InputStream> ideStreams = new TreeMap<>();
+    File files[] = (new File(folder)).listFiles();
+    for (File lib: files) {
+      String libName = lib.getName();
+      InputStream iStream = new FileInputStream(lib);
+      ideStreams.put(libName, iStream);
+    }
+
+    return ideStreams;
   }
 }
