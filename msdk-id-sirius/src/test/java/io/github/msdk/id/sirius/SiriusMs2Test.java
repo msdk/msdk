c2d5c2919c279d5de20606ffa6e085e0f7e4d456
Evgeny Sorokin <evgerher1l@gmail.com>
2018-06-25 19:31:51 +0300

    Remove license header from tests

--- msdk-id-sirius/src/test/java/io/github/msdk/id/sirius/SiriusMs2Test.java
+++ msdk-id-sirius/src/test/java/io/github/msdk/id/sirius/SiriusMs2Test.java
@@ -1,15 +1,4 @@
-package io.github.msdk.id.sirius;/*
- * (C) Copyright 2015-2017 by MSDK Development Team
- *
- * This software is dual-licensed under either
- *
- * (a) the terms of the GNU Lesser General Public License version 2.1 as published by the Free
- * Software Foundation
- *
- * or (per the licensee's choosing)
- *
- * (b) the terms of the Eclipse Public License v1.0 as published by the Eclipse Foundation.
- */
+package io.github.msdk.id.sirius;
 
 import de.unijena.bioinf.ChemistryBase.chem.FormulaConstraints;
 import de.unijena.bioinf.sirius.IdentificationResult;
