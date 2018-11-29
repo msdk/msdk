b87e4963bde6f27c5587e2a199af7fb1bf60c194
Tomas Pluskal <plusik@gmail.com>
2018-11-29 15:13:15 -0500

    Fixed charge processing in MGF import

--- msdk-io-mgf/src/main/java/io/github/msdk/io/mgf/MgfFileImportMethod.java
+++ msdk-io-mgf/src/main/java/io/github/msdk/io/mgf/MgfFileImportMethod.java
@@ -13,16 +13,6 @@
 
 package io.github.msdk.io.mgf;
 
-import io.github.msdk.MSDKException;
-import io.github.msdk.MSDKMethod;
-import io.github.msdk.MSDKRuntimeException;
-import io.github.msdk.datamodel.MsSpectrumType;
-import io.github.msdk.spectra.centroidprofiledetection.SpectrumTypeDetectionAlgorithm;
-import io.github.msdk.util.ArrayUtil;
-import io.github.msdk.util.DataPointSorter;
-import io.github.msdk.util.DataPointSorter.SortingDirection;
-import io.github.msdk.util.DataPointSorter.SortingProperty;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -38,14 +28,25 @@ import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.github.msdk.MSDKException;
+import io.github.msdk.MSDKMethod;
+import io.github.msdk.datamodel.MsSpectrumType;
+import io.github.msdk.spectra.centroidprofiledetection.SpectrumTypeDetectionAlgorithm;
+import io.github.msdk.util.ArrayUtil;
+import io.github.msdk.util.DataPointSorter;
+import io.github.msdk.util.DataPointSorter.SortingDirection;
+import io.github.msdk.util.DataPointSorter.SortingProperty;
+
 /**
- * <p>MgfFileImportMethod class.</p>
+ * <p>
+ * MgfFileImportMethod class.
+ * </p>
  *
- *         This class parses a list of MgfMsSpectrums from .mgf file
+ * This class parses a list of MgfMsSpectrums from .mgf file
  */
 public class MgfFileImportMethod implements MSDKMethod<List<MgfMsSpectrum>> {
   private final Pattern PEPMASS_PATTERN = Pattern.compile("(?<=PEPMASS=)(\\d+\\.\\d+)");
-  private final Pattern CHARGE_PATTERN = Pattern.compile("(?<=CHARGE=)(\\d+)\\+|(\\d+)-");
+  private final Pattern CHARGE_PATTERN = Pattern.compile("^CHARGE=(\\d+)([+-]?)");
   private final Pattern TITLE_PATTERN = Pattern.compile("(?<=TITLE=).*");
   private final Pattern LEVEL_PATTERN = Pattern.compile("(?<=MSLEVEL=).*");
   private final @Nonnull File target;
@@ -56,8 +57,10 @@ public class MgfFileImportMethod implements MSDKMethod<List<MgfMsSpectrum>> {
 
   /**
    * <p>
-   *   Overloaded method, processes the .mgf file (loaded in the costructor) and returns a list of MgfMsSpectrums
+   * Overloaded method, processes the .mgf file (loaded in the costructor) and returns a list of
+   * MgfMsSpectrums
    * </p>
+   * 
    * @throws MSDKException if any
    */
   @Nullable
@@ -90,7 +93,10 @@ public class MgfFileImportMethod implements MSDKMethod<List<MgfMsSpectrum>> {
   }
 
   /**
-   * <p>Method for processing .mgf files from BEGIN IONS till END IONS</p>
+   * <p>
+   * Method for processing .mgf files from BEGIN IONS till END IONS
+   * </p>
+   * 
    * @param reader - the reader with already accepted .mgf file
    * @return the new MgfMsSpectrum object
    * @throws IOException if any
@@ -122,52 +128,64 @@ public class MgfFileImportMethod implements MSDKMethod<List<MgfMsSpectrum>> {
         if (line.contains("PEPMASS")) {
           matched = matchPattern(line, PEPMASS_PATTERN);
           precursorMass = Double.parseDouble(matched);
-        } else if (line.contains("TITLE")) {
+          continue;
+        }
+
+        if (line.contains("TITLE")) {
           title = matchPattern(line, TITLE_PATTERN);
-        } else if (line.contains("MSLEVEL")) {
+          continue;
+        }
+
+        if (line.contains("MSLEVEL")) {
           matched = matchPattern(line, LEVEL_PATTERN);
           mslevel = Integer.parseInt(matched);
-        } else if (line.contains("CHARGE")) {
-          matched = matchPattern(line, CHARGE_PATTERN);
+          continue;
+        }
 
-          // Remove + or - from the end of the string to get int value
-          String unsignedIntString = matched.substring(0, matched.length() - 1);
-          precursorCharge = Integer.parseInt(unsignedIntString);
+        Matcher m = CHARGE_PATTERN.matcher(line);
+        if (m.find()) {
+          String chargeString = m.group(1);
+          precursorCharge = Integer.parseInt(chargeString);
 
           // Check if negative charge
-          char sign = matched.charAt(matched.length() - 1);
-          if (sign == '-') {
+          String sign = m.group(2);
+          if (sign.equals("-")) {
             precursorCharge *= -1;
           }
-        } else {
-          String[] floats = line.split(" ");
 
-          // Check that line is two columned
-          if (floats.length != 2) {
-            continue;
-          }
+          continue;
 
-          try { // Catch exception if tries to parse non-float
-            double mzValue = Double.parseDouble(floats[0]);
-            float intensityValue = Float.parseFloat(floats[1]);
-            mz = ArrayUtil.addToArray(mz, mzValue, index);
-            intensity = ArrayUtil.addToArray(intensity, intensityValue, index);
-          } catch (NumberFormatException e) {
-            logger.debug("Failed to parse floats from string {}", index);
-          } finally {
-            index++;
-          }
         }
+
+        String[] floats = line.split(" ");
+
+        // Check that line is two columned
+        if (floats.length != 2) {
+          continue;
+        }
+
+        try { // Catch exception if tries to parse non-float
+          double mzValue = Double.parseDouble(floats[0]);
+          float intensityValue = Float.parseFloat(floats[1]);
+          mz = ArrayUtil.addToArray(mz, mzValue, index);
+          intensity = ArrayUtil.addToArray(intensity, intensityValue, index);
+        } catch (NumberFormatException e) {
+          logger.debug("Failed to parse floats from string {}", index);
+        } finally {
+          index++;
+        }
+
       } catch (IllegalStateException e) {
-        logger.debug("Regex could not recognize the pattern :: {}", line);
+        logger.debug("Regex could not recognize the pattern {}", line);
         throw new MSDKException("Incorrect data format", e);
       }
     }
 
 
-  // Auto detect the type of spectrum and sort mz values
+    // Auto detect the type of spectrum and sort mz values
     MsSpectrumType type = SpectrumTypeDetectionAlgorithm.detectSpectrumType(mz, intensity, index);
-    DataPointSorter.sortDataPoints(mz, intensity, index, SortingProperty.MZ, SortingDirection.ASCENDING);
+    DataPointSorter.sortDataPoints(mz, intensity, index, SortingProperty.MZ,
+        SortingDirection.ASCENDING);
 
     MgfMsSpectrum spectrum = new MgfMsSpectrum(mz, intensity, index, type);
     spectrum.setMsLevel(mslevel);
@@ -178,7 +196,10 @@ public class MgfFileImportMethod implements MSDKMethod<List<MgfMsSpectrum>> {
   }
 
   /**
-   * <p> Method for matching the string with required pattern </p>
+   * <p>
+   * Method for matching the string with required pattern
+   * </p>
+   * 
    * @param line - new line from reader object
    * @param pattern - pattern to be found in a line
    * @return - matched pattern
@@ -223,6 +244,7 @@ public class MgfFileImportMethod implements MSDKMethod<List<MgfMsSpectrum>> {
    * </p>
    *
    * Initializes regex dictionary and loads file
+   * 
    * @param target .mgf file to be processed
    */
   public MgfFileImportMethod(File target) {
